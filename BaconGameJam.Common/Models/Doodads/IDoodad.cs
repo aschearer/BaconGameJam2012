721752180cb8c307b71c1a2df765ad7728b3f46d
Matt Mullins <mattmullins@gmail.com>
2012-08-04 17:28:56 -0400

    Red tanks moving up/down

--- BaconGameJam.Common/Models/Doodads/IDoodad.cs
+++ BaconGameJam.Common/Models/Doodads/IDoodad.cs
@@ -1,7 +1,10 @@
+using Microsoft.Xna.Framework;
+
 namespace BaconGameJam.Common.Models.Doodads
 {
     public interface IDoodad
     {
         void RemoveFromGame();
+        void Update(GameTime gameTime);
     }
 }
--- BaconGameJam.Common/Models/Doodads/Tank.cs
+++ BaconGameJam.Common/Models/Doodads/Tank.cs
@@ -22,12 +22,14 @@ namespace BaconGameJam.Common.Models.Doodads
             this.body.BodyType = BodyType.Kinematic;
             this.Team = team;
             this.Heading = rotation;
+            this.MovingUp = true;
 
             var shape = new PolygonShape(0);
             shape.SetAsBox(15 / Constants.PixelsPerMeter, 15 / Constants.PixelsPerMeter);
             var fixture = this.body.CreateFixture(shape);
             fixture.CollisionCategories = this.CollisionCategory;
             fixture.CollidesWith = Constants.EnemyCategory | Constants.ObstacleCategory | Constants.MissileCategory;
+
         }
 
         public bool IsMoving { get; private set; }
@@ -46,13 +48,46 @@ namespace BaconGameJam.Common.Models.Doodads
 
         public float Heading { get; protected set; }
 
+        public bool MovingUp { get; protected set; }
+        public bool MovingLeft { get; protected set; }
+
         protected virtual Category CollisionCategory
         {
             get { return Constants.EnemyCategory; }
         }
 
+        bool Body_OnCollision(Fixture fixtureA, Fixture fixtureB, FarseerPhysics.Dynamics.Contacts.Contact contact)
+        {
+            if (fixtureB.CollisionCategories == Constants.ObstacleCategory)
+            {
+                if ((this.Heading >= 0) && (this.Heading <= 180))
+                    this.Heading += 180;
+                else if ((this.Heading > 180) && (this.Heading <= 360))
+                    this.Heading -= 180;
+            }
+
+            return true;
+        }
+
         public void Update(GameTime gameTime)
         {
+            if (this.Team == Doodads.Team.Red)
+            {
+                Vector2 rayStart = new Vector2(Position.X, Position.Y);
+                Vector2 rayEnd = rayStart + new Vector2(0, (MovingUp ? -1 : 1));
+
+                this.world.RayCast((fixture, point, normal, fraction) =>
+                    {
+                        if (fixture != null)
+                        {
+                            MovingUp = !MovingUp;
+                            return 1;
+                        }
+                        return fraction;
+                    }, rayStart, rayEnd);
+
+                this.body.SetTransform(new Vector2(this.body.Position.X, this.body.Position.Y + (this.MovingUp ? -0.01f : 0.01f)), this.Heading);
+            }
         }
 
         public void RemoveFromGame()
